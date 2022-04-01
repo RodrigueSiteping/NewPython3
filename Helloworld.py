e172130824b04ac8fdc4dad3ebda791cca9693c3
rodri <rodriguesiteping@gmail.com>
2022-04-01 06:35:27 +0200

    1. envoi fichier Helloworld.py

---
+++ Helloworld.py
@@ -0,0 +1,20 @@
+print ('1- first line')
+print ('2- second line! \n la souris fuit' )
+
+print('''3- Histoire!
+le chat arrive''')
+
+print('4- voici double quote " '+ " here is a single quote ' ")
+
+print("5- ou tu peux juste faire ceci \" cela fonctionne \"")
+print( "6- puis-je afficher un \ on a screen?")
+
+#insere un /\\ pour afficher  un \
+print( "7- j'afficher un double \\\ on a screen? \n")
+
+
+
+print ('Histoire Dickory Dock.')
+print("c'est juste un petit monde. ")
+print("Hi there'")
+print ('Hello Armel! \n')
