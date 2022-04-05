6a0109e91ea777d8b4aeb8962198b214ac08cded
rodri <rodriguesiteping@gmail.com>
2022-04-05 16:02:00 +0200

    modification de Helloworld.py et repoPythonBook2020.code-workspace

--- Helloworld.py
+++ Helloworld.py
@@ -1,20 +1,40 @@
-print ('1- first line')
-print ('2- second line! \n la souris fuit' )
+
+
+print('1- first line')
+
+print('2- second line! \n la souris fuit')
 
 print('''3- Histoire!
 le chat arrive''')
 
-print('4- voici double quote " '+ " here is a single quote ' ")
+print('4- voici double quote " ' + " here is a single quote ' ")
 
 print("5- ou tu peux juste faire ceci \" cela fonctionne \"")
-print( "6- puis-je afficher un \ on a screen?")
+print("6- puis-je afficher un ' \ ' on a screen?")
 
 #insere un /\\ pour afficher  un \
-print( "7- j'afficher un double \\\ on a screen? \n")
-
+print("7- j'afficher un double \\\ on a screen? \n")
 
 
 print ('Histoire Dickory Dock.')
 print("c'est juste un petit monde. ")
 print("Hi there'")
 print ('Hello Armel! \n')
+
+# This is a Python comment in my first Python app.
+# This variable contains an integer
+quantity = 14
+# This variable contains a float
+unit_price = 26.99
+# This variable contains the result of multiplying quantity times unit price
+extended_price = quantity * unit_price
+# Show some results on the screen.
+print(extended_price)
+
+# Petit code
+x = 10
+if x == 0:
+    print("x is zero")
+else:
+    print("x is ",x)
+print("All done")
